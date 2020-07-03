c11cc2722cec7ed27ec3edf4742cc06e8390bc8b
Dr. Farhana Akter <61399350+fakter777@users.noreply.github.com>
2020-07-03 05:05:35 -0400

    Delete serializers.py

--- serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UserAPI
-
-
-class UserApiSerializer(serializers.Serializer);
-    name = serializers.CharField(max_lenght=50)
-    email = serializers.Charfield()
-    password = serializers.Charfield()
